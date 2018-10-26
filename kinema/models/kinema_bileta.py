e4ef5873c7234970173c4d40a1be1554f272213a
endraceni <44263329+endraceni@users.noreply.github.com>
2018-10-26 15:00:09 +0200

    Add files via upload

--- kinema/models/kinema_bileta.py
+++ kinema/models/kinema_bileta.py
@@ -2,7 +2,7 @@ from odoo import api, fields, models
 from odoo import tools, _
 from odoo.exceptions import ValidationError, UserError
 from odoo.modules.module import get_module_resource
-import datetime
+from datetime import datetime
 
 
 class Salla (models.Model):
@@ -26,6 +26,9 @@ class Shfaqje(models.Model):
         ora_perfundimit = fields.Datetime(string="Orari perfundimit", required=True)
         state = fields.Selection([('aktive', 'Aktive'), ('draft', 'Draft'), ('anulluar', 'Anulluar'), ('perfunduar', 'Perfunduar')],default="draft", string="State")
         bileta_id = fields.One2many('bileta.bileta', 'shfaqje_id', string="Bileta")
+        bileta_sh = fields.Integer(string = "Bileta shitur: ", compute = "bileta_shitur")
+        bileta_gjendje = fields.Integer(string = "Bileta ne gjendje: ",compute = "bileta_shitur")
+
 
 
         # Gjenero Bileta
@@ -41,8 +44,7 @@ class Shfaqje(models.Model):
         def cancel_show(self):
             for shfaqje in self:
                 if shfaqje.id_salla.kapaciteti:
-                    self.env['bileta.bileta'].search(
-                        ['&', ('shfaqje_id', '=', shfaqje.id), ('state', '!=', 'konfirmuar')]).unlink()
+                    self.env['bileta.bileta'].search(['&', ('shfaqje_id', '=', shfaqje.id), ('state', '!=', 'konfirmuar')]).unlink()
 
         @api.one
         def draft(self):
@@ -52,19 +54,32 @@ class Shfaqje(models.Model):
         @api.one
         def aktive(self):
             self.state = 'aktive'
+            self.id_salla.state = 'e zene'
             self.gjenero_bileta()
 
         @api.one
         def anulluar(self):
             self.state = 'anulluar'
+            self.id_salla.state = 'e lire'
             self.cancel_show()
 
         @api.one
         def perfunduar(self):
             self.state = 'perfunduar'
+            self.id_salla.state = 'e lire'
             for bileta in self:
                 if bileta.bileta_id:
                     self.bileta_id.write({'state': 'perfunduar'})
+
+        @api.multi
+        def bileta_shitur(self):
+            for shfaqje in self:
+                s= 0
+                if shfaqje.id_salla.kapaciteti:
+                   s = shfaqje.env['bileta.bileta'].search_count([('state','=','konfirmuar')])
+                self.bileta_sh = s
+                self.bileta_gjendje = shfaqje.id_salla.kapaciteti - s
+
 
 class Bileta(models.Model):
         _name = "bileta.bileta"
@@ -90,7 +105,6 @@ class Bileta(models.Model):
             }
 
 
-
 class Rezervim(models.Model):
         _name = "rezervim.filmi"
         _description = "Rezervimi Biletes"
@@ -101,7 +115,7 @@ class Rezervim(models.Model):
         orar_perfundimit = fields.Datetime(related='shfaqje_id.ora_perfundimit', string="Orari i perfundimit Shfaqjes",readonly=True)
         bileta_id = fields.Many2one('bileta.bileta', string="Bileta")
         bileta_nr = fields.Char(related="bileta_id.serial_no", string="Nr Serial", readonly=True)
-        payed = fields.Boolean(string="Payed")
+        payed = fields.Boolean(string="Paid")
         afati_anullimit = fields.Integer(string='Afati i anullimit')
         state = fields.Selection([('regjistruar', 'Regjistruar'), ('rezervuar', 'Rezervo'), ('konfirmuar', 'Konfirmo'), ('anulluar', 'Anullo')],default='regjistruar', string="State")
 
@@ -112,24 +126,12 @@ class Rezervim(models.Model):
             vals['emer_rezervimi'] = seq
             return super(Rezervim, self).create(vals)
 
-
-
-        # @api.onchange
-        # def ndrysho_(self,val):
-        #     for bileta in self:
-        #         if bileta.shfaqje_id:
-        #             if bileta.shfaqje_id.bileta_id:
-        #                 nr = self.env['bileta.bileta'].search(('serial_no','=',bileta.shfaqje_id.bileta_id))
-        #                 if nr:
-        #                     self.bileta_nr = nr
-        #                       val['bileta_nr']= nr
-        #                     return super(Rezervim, self).write(val)
-
-
-
         @api.one
         def regjistruar(self):
             self.state = 'regjistruar'
+            for bileta in self:
+                if bileta.bileta_id:
+                    self.bileta_id.write({'state': 'rezervuar'})
 
         @api.one
         def rezervuar(self):
@@ -142,18 +144,42 @@ class Rezervim(models.Model):
         def konfirmuar(self):
             if self.payed == True:
                 self.state = 'konfirmuar'
-                self.env['bileta.bileta'].write({'state': 'konfirmuar'})
+                for bileta in self:
+                    if bileta.bileta_id:
+                        self.bileta_id.write({'state': 'konfirmuar'})
             else:
-                raise UserWarning('JU nuk mund ta konfirmoni rezervimin nqs nuk keni paguar')
+                raise UserError('JU nuk mund ta konfirmoni rezervimin nqs nuk keni paguar')
+
+
+        # @api.one
+        # def anulluar(self):
+        #     if self.state == 'konfirmuar':
+        #         current_date = datetime.date.today()
+        #         if self.orar_fillimi:
+        #             if self.orar_fillimi - current_date < '3':
+        #                 raise UserError('Ju nuk mund te anulloni rezervimin tuaj')
+        #             else:
+        #                 self.state = 'anulluar'
+        #                 for bileta in self:
+        #                     if bileta.bileta_id:
+        #                         self.bileta_id.write({'state': 'anulluar'})
+
 
 
         @api.one
         def anulluar(self):
             if self.state == 'konfirmuar':
-                current_date = datetime.date.today()
-                if self.orar_fillimi:
-                    if self.orar_fillimi - current_date < '3':
-                        raise UserWarning('Ju nuk mund te anulloni rezervimin tuaj')
-                    else:
-                        self.state = 'anulluar'
-                        self.bileta_id.write({'state':'anulluar'})
+
+                fmt = '%Y-%m-%d %H:%M:%S'
+                from_date = self.orar_fillimi
+                # d2 = (datetime.today()).strftime(fmt)
+                # d1 = datetime.strptime(from_date, fmt).date()
+                # daysDiff = (d1 - d2).days
+                # dayss = datetime.strptime(daysDiff)
+                # raise ValidationError(dayss)
+                    #     raise UserError('Ju nuk mund te anulloni rezervimin tuaj')
+                    # else:
+                    #     self.state = 'anulluar'
+                    #     for bileta in self:
+                    #         if bileta.bileta_id:
+                    #             self.bileta_id.write({'state': 'anulluar'})
